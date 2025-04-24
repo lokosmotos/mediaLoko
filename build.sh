7e8288ebbf96c4a6b8aec73b8077d8fc0e0a0cbf
lokosmotos <danial.khalif.dk@gmail.com>
2025-04-24 14:32:09 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 # Install system dependencies
@@ -8,8 +7,7 @@ apt-get install -y \
     tesseract-ocr \
     libtesseract-dev \
     libleptonica-dev \
-    tesseract-ocr-eng \  # English language pack
-    tesseract-ocr-all   # All language packs (optional)
+    tesseract-ocr-eng
 
 # Install Python dependencies
 pip install --upgrade pip
