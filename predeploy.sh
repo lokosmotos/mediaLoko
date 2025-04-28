0e44ce965fcb70c2e36ce610a905a98e95a0fc4e
lokosmotos <danial.khalif.dk@gmail.com>
2025-04-28 15:41:56 +0800

    Delete predeploy.sh

--- predeploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install --upgrade pip
-pip install numpy==1.23.5 --force-reinstall
-pip install -r requirements.txt
-python -m spacy download en_core_web_sm
-python -m spacy download zh_core_web_sm
