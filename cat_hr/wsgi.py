6931d1320cecb073d9d896d40b0fac1bd324f35d
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-02 14:55:51 +0800

    Update wsgi.py

--- cat_hr/wsgi.py
+++ cat_hr/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cat_hr.settings')  # ← Match your project name
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cat_hr.settings")
 application = get_wsgi_application()
