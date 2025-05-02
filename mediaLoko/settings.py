7c2fbdb8f2bda682e1660b958cd55eceb375581e
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-02 16:19:41 +0800

    Add files via upload

---
+++ mediaLoko/settings.py
@@ -0,0 +1,16 @@
+from decouple import config
+import dj_database_url
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
